c53cc18413dc03a96f78705f29b759f86959c2c2
Aldair <aldairgarcia803@gmail.com>
2019-03-05 00:54:12 +0000

    css

--- js/test.js
+++ js/test.js
@@ -23,9 +23,24 @@ $(".mario").click(function(){
 $(".school").click(function(){
     window.open("https://jalvarez2016.github.io/Compture-science-class/");
 });
-$("#navbutton").click(function() {
-    var number=0;
-    var timescliked=number+1;
-    console.log(timescliked);
-    $("#navv").css("height","200px");
-})
+
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1;}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
