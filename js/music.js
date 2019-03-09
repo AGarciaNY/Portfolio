5c8173855997b645d2cea21f6e4d7b01a8c56e5e
Aldair <aldairgarcia803@gmail.com>
2019-03-09 22:07:56 +0000

    css

--- js/music.js
+++ js/music.js
@@ -1,4 +1,6 @@
 /*global $*/
+/*global firebase*/
+
 
 
 var x = document.getElementById("myAudio");
@@ -26,3 +28,41 @@ $(".playstop").click(function(){
         disableControls();
         }
     });
+    
+var database = firebase.database();
+var databaseRef = database.ref("/");
+
+// Read the data from the database and take a snapshot of that data.
+databaseRef.once("value").then(function(snapshot) {
+ // Use .val() to get the data from the snapshot.
+ const directory = snapshot.val();
+ console.log(directory);
+    for(var key in directory){
+        console.log(key);
+        console.log(directory[key].name);
+        var uname = directory[key].name.name;
+        var umail = directory[key].name.mail;
+        var ucomment = directory[key].name.comment;
+        console.log(uname);
+        console.log(umail);
+        console.log(ucomment);
+    }
+});
+
+$("#submit").click(function(){
+            var name1=$("#viewername").val();
+            var mail1=$("#viewermail").val();
+            var comment1=$("#viewercoment").val();
+            var scomment=comment1.split(" ");
+            var comle=scomment.length;
+            
+            if(comle >10){
+                alert("no");
+            }else{
+                databaseRef.push({
+                    name:name1,
+                    mail:mail1,
+                    comment:comment1
+                });
+            }
+});
--- js/test.js
+++ js/test.js
@@ -27,7 +27,7 @@ $(".school").click(function(){
 
 
 var slideIndex = 0;
-showSlides();
+
 
 function showSlides() {
   var i;
@@ -45,43 +45,4 @@ function showSlides() {
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
-
-var database = firebase.database();
-var databaseRef = database.ref("/");
-
-// Read the data from the database and take a snapshot of that data.
-databaseRef.once("value").then(function(snapshot) {
- // Use .val() to get the data from the snapshot.
- const directory = snapshot.val();
- console.log(directory)
- var stories = $("#stories")
-    for(var key in directory){
-        console.log(key);
-        console.log(directory[key].age);
-        var age = directory[key].age
-        var title = directory[key].title
-        var story = directory[key].story
-        var country = directory[key].country
-       // var div = $("<div>").text(" age: " + age + " country: " + country + " title: " + title + " story: " + story );
-      //  $("#stories").append(div);
-        var outerdiv = $("<div class='col-lg-4 col-sm-6 portfolio-item'>");
-        var carddiv = $("<div class='card h-100'>");
-        var bodydiv = $("<div class='card-body'>");
-        var titleh4 = $("<h4 class= 'card-title'>").text(title);
-        var storyp = $("<p class='card-text'>").text(story);
-        bodydiv.append(titleh4, storyp);
-        carddiv.append(bodydiv);
-        outerdiv.append(carddiv);
-        $("#stories").append(outerdiv);
-    }
-});
-$("#submit").click(function(){
-        //$("#age").val()
-        databaseRef.push({
-            age:$("#age").val(),
-            country:$("#country").val(),
-            story:$("#story").val(),
-            title:$("#title").val()
-        })
-}   
-    );
+showSlides();
