595c699853186df67ee1bd6f0228814a1b967b2d
Aldair <aldairgarcia803@gmail.com>
2019-03-10 19:25:20 +0000

    cool stuf

--- js/music.js
+++ js/music.js
@@ -1,6 +1,6 @@
 /*global $*/
 /*global firebase*/
-
+/*glodal location*/
 
 
 var x = document.getElementById("myAudio");
@@ -40,15 +40,20 @@ databaseRef.once("value").then(function(snapshot) {
     for(var key in directory){
         console.log(key);
         var uname = directory[key].name;
-        var umail = directory[key].mail;
+        // var umail = directory[key].mail;
         var ucomment = directory[key].comment;
-        $("#commentsbox").html(
-            "<div>"+
-            "<h1>himan</h1>"
-        
+        $(".commentsbox").append(
+            "<div class='comentholder'>"+
+            "<h3 class='postedname'>Comment by:"+uname+"</h3>"+
+            "<div class='postedcomm2ent'>"+
+                "<h4>"+ucomment+"</h4>"+
+            "</div>"+
+            "<div style='height:10px;'></div>"+
+            "<hr style='height:.1px;border:.5px solid black;'>"+
+        "</div>"
             );
     }
-});
+
 
 $("#submit").click(function(){
             var name1=$("#viewername").val();
@@ -63,7 +68,7 @@ $("#submit").click(function(){
             var maillegth=smail.length;
             var comle=scomment.length;
             
-            if(comle < 10 || maillegth<4  || namelegth<4){
+            if(comle < 10 || maillegth<3  || namelegth<3){
                 alert("no");
             }else{
                 databaseRef.push({
@@ -71,6 +76,8 @@ $("#submit").click(function(){
                     mail:mail1,
                     comment:comment1
                 });
+                location.reload();
             }
     
+    });
 });
